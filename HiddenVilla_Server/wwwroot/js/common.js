f2e25803d3b00660f2d2cbf0d0b6043c264c586e
RositsaRuseva <rossyplruseva@gmail.com>
2022-07-22 07:22:18 +0300

    Initial commit

---
+++ HiddenVilla_Server/wwwroot/js/common.js
@@ -0,0 +1,24 @@
+﻿window.ShowToastr = (type, message) => {
+    if (type === "success") {
+        toastr.success(message, "Operation Successful")
+    }
+    if (type === "error") {
+        toastr.error(message, "Operation Failed")
+    }
+}
+window.ShowSweetAlert = (type, message) => {
+    if (type === "success") {
+        Swal.fire(
+            'Success notification',
+            message,
+            'success'
+        )
+    }
+    if (type === "error") {
+        Swal.fire(
+            'Error notification',
+            message,
+            'error'
+        )
+    }
+}
