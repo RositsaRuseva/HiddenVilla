ffcc37d4af556e12ead82750ad96cc3a26b508f4
RositsaRuseva <rossyplruseva@gmail.com>
2022-09-06 15:52:58 +0300

    Setup Account Controller

---
+++ HiddenVilla_API/Controllers/AccountController.cs
@@ -0,0 +1,25 @@
+﻿using DataAcess.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HiddenVilla_API.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    [Authorize]
+    public class AccountController : ControllerBase
+    {
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public AccountController(SignInManager<ApplicationUser> signInManager, UserManager<ApplicationUser> userManager,
+            RoleManager<IdentityRole> roleManager)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+    }
+}
