f451df7dd82cb3b0963b51216e5e550fbea83ed4
RositsaRuseva <rossyplruseva@gmail.com>
2022-09-06 15:29:46 +0300

    Get Hotel Room EndPoint

--- HiddenVilla_API/Controllers/HotelRoomController.cs
+++ HiddenVilla_API/Controllers/HotelRoomController.cs
@@ -1,5 +1,6 @@
 ﻿using Business.Repository.IRepository;
 using Microsoft.AspNetCore.Mvc;
+using Models;
 
 namespace HiddenVilla_API.Controllers
 {
@@ -22,6 +23,30 @@ namespace HiddenVilla_API.Controllers
         {
             var allRooms = await _hotelRoomRepository.GetAllHotelRoom();
             return Ok(allRooms);
+        }
+        [HttpGet("{roomId}")]
+        public async Task<IActionResult> GetHotelRoom(int? roomId)
+        {
+            if (roomId == null)
+            {
+                return BadRequest(new ErrorModel()
+                {
+                    Title = "",
+                    ErrorMessage = "Invalid Room Id",
+                    StatusCode = StatusCodes.Status400BadRequest
+                });
+            }
+            var roomDetails = await _hotelRoomRepository.GetHotelRoom(roomId.Value);
+            if (roomDetails == null)
+            {
+                return BadRequest(new ErrorModel()
+                {
+                    Title = "",
+                    ErrorMessage = "Invalid Room Id",
+                    StatusCode = StatusCodes.Status404NotFound
+                });
+            }
+            return Ok(roomDetails);
         }
     }
 }
