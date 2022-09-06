d388c5e8f667816b9c28f8d5fa86fe0399d5975f
RositsaRuseva <rossyplruseva@gmail.com>
2022-09-06 08:55:56 +0300

    HotelRoom Controller

---
+++ HiddenVilla_API/Controllers/HotelRoomController.cs
@@ -0,0 +1,27 @@
+﻿using Business.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HiddenVilla_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HotelRoomController : ControllerBase
+    {
+        private readonly IHotelRoomRepository _hotelRoomRepository;
+
+        public HotelRoomController(IHotelRoomRepository hotelRoomRepository)
+        {
+            _hotelRoomRepository = hotelRoomRepository;
+        }
+        /// <summary>
+        /// Gets All hotel rooms
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public async Task<IActionResult> GetHotelRooms()
+        {
+            var allRooms = await _hotelRoomRepository.GetAllHotelRoom();
+            return Ok(allRooms);
+        }
+    }
+}
