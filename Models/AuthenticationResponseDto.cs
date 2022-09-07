8188b59b3d9869ea143ae790c5f5e3f3c3d99812
RositsaRuseva <rossyplruseva@gmail.com>
2022-09-07 07:19:22 +0300

    SignIn Dto

---
+++ Models/AuthenticationResponseDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class AuthenticationResponseDto
+    {
+        public bool IsAuthenticationSuccessful { get; set; }
+        public IEnumerable<string> Errors { get; set; }
+        public string Token { get; set; }
+        public UserDto UserDto { get; set; }
+    }
+}
