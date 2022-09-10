b7f9cfd4544b7c233ca046eeb79f9ea8a3c40826
RositsaRuseva <rossyplruseva@gmail.com>
2022-09-10 08:21:51 +0300

    Home View Model

---
+++ HiddenVilla_Client/Model/ViewModel/HomeViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace HiddenVilla_Client.Model.ViewModel
+{
+    public class HomeViewModel
+    {
+        public DateTime StartDate { get; set; } = DateTime.Now;    
+        public DateTime EndDate { get; set; }
+        public int NumberOfNights { get; set; } = 1;
+    }
+}
