dd55010a37064158c5c34c28498454270ef1d959
RositsaRuseva <rossyplruseva@gmail.com>
2022-09-26 22:49:23 +0300

    Room Order Endpoint in API

---
+++ HiddenVilla_API/Controllers/RoomOrderController.cs
@@ -0,0 +1,33 @@
+﻿using Business.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc;
+using Models;
+
+namespace HiddenVilla_API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public class RoomOrderController : Controller
+    {
+        private readonly IRoomOrderDetailsRepository _repository;
+        public RoomOrderController(IRoomOrderDetailsRepository repository)
+        {
+            _repository = repository;
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] RoomOrderDetailsDto details)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _repository.Create(details);
+                return Ok(result);
+            }
+            else
+            {
+                return BadRequest(new ErrorModel()
+                {
+                    ErrorMessage = "Error while creating Room Details/ Booking"
+                });
+            }
+        }
+    }
+}
--- HiddenVilla_Client/Service/RoomOrderDetailsService.cs
+++ HiddenVilla_Client/Service/RoomOrderDetailsService.cs
@@ -1,5 +1,7 @@
 ﻿using HiddenVilla_Client.Service.IService;
 using Models;
+using Newtonsoft.Json;
+using System.Text;
 
 namespace HiddenVilla_Client.Service
 {
@@ -16,9 +18,23 @@ namespace HiddenVilla_Client.Service
             throw new NotImplementedException();
         }
 
-        public Task<RoomOrderDetailsDto> SaveRoomOrderDetails(RoomOrderDetailsDto roomOrderDetailsDto)
+        public async Task<RoomOrderDetailsDto> SaveRoomOrderDetails(RoomOrderDetailsDto roomOrderDetailsDto)
         {
-            throw new NotImplementedException();
+            var content = JsonConvert.SerializeObject(roomOrderDetailsDto);
+            var bodyContent = new StringContent(content, Encoding.UTF8, "application/json");
+            var response = await _client.PostAsync("api/roomorder/create", bodyContent);
+            if (response.IsSuccessStatusCode)
+            {
+                var contentTemp = await response.Content.ReadAsStringAsync();
+                var result = JsonConvert.DeserializeObject<RoomOrderDetailsDto>(contentTemp);
+                return result;
+            }
+            else
+            {
+                var contentTemp = await response.Content.ReadAsStringAsync();
+                var errorModel = JsonConvert.DeserializeObject<ErrorModel>(contentTemp);
+                throw new Exception(errorModel.ErrorMessage);
+            }
         }
     }
 }
