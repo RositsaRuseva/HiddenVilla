b7368b6940287c9ce6d0cc2c2450d1a0c1f6d180
RositsaRuseva <rossyplruseva@gmail.com>
2022-09-27 23:58:36 +0300

    Invoke Stripe

---
+++ HiddenVilla_Client/wwwroot/js/StripePayment.js
@@ -0,0 +1,6 @@
+﻿redirectToCheckout = function (sessionId) {
+    var stripe = Stripe('pk_test_51LmKXOAjAcWJ2RFGvntAfFXYBvTb9p4MmbZPrSbAuK6LyfaUA7Aq0N86Jd5F538jYAyRdIhp6kUb7OFvjiIT3aed00wuCn6wQ8');
+    stripe.redirectToCheckOut({
+        sessionId: sessionId
+    });
+};
