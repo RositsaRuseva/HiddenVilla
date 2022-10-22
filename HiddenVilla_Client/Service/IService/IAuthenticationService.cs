9e84569ca530c96f8e3bea0b8b261d7f103aa28e
RositsaRuseva <rossyplruseva@gmail.com>
2022-10-22 08:00:43 +0300

    IAuthenticationService

---
+++ HiddenVilla_Client/Service/IService/IAuthenticationService.cs
@@ -0,0 +1,11 @@
+﻿using Models;
+
+namespace HiddenVilla_Client.Service.IService
+{
+    public interface IAuthenticationService
+    {
+        Task<RegistrationResponseDto> RegisterUser(UserRequestDto userForRegistration);
+        Task<AuthenticationResponseDto> Login(AuthenticationDto userFromAuthentication);
+        Task Logout();
+    }
+}
